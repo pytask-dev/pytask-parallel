2051c5020074a2498f5a683ce9b0aa88a6d50c9d
Tobias Raabe <raabe@posteo.de>
2020-08-13 18:53:00 +0200

    Better config parsing.

--- src/pytask_parallel/config.py
+++ src/pytask_parallel/config.py
@@ -11,13 +11,17 @@ def pytask_parse_config(config, config_from_cli, config_from_file):
     )
     if config["n_workers"] == "auto":
         config["n_workers"] = max(os.cpu_count() - 1, 1)
+    else:
+        config["n_workers"] = int(config["n_workers"])
     config["delay"] = get_first_not_none_value(
-        config_from_cli, config_from_file, key="delay", default=0.1
+        config_from_cli, config_from_file, key="delay", default=0.1, callback=float
     )
 
     config["parallel_backend"] = get_first_not_none_value(
         config_from_cli, config_from_file, key="parallel_backend", default="processes"
     )
+    if config["parallel_backend"] not in ["processes", "threads"]:
+        raise ValueError("parallel_backend has to be 'processes' or 'threads'.")
 
 
 @hookimpl
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,4 +1,5 @@
 import os
+import textwrap
 
 import pytest
 from pytask import main
@@ -18,3 +19,35 @@ from pytask import main
 def test_interplay_between_debugging_and_parallel(tmp_path, pdb, n_workers, expected):
     session = main({"paths": tmp_path, "pdb": pdb, "n_workers": n_workers})
     assert session.config["n_workers"] == expected
+
+
+@pytest.mark.parametrize("config_file", ["pytask.ini", "tox.ini", "setup.cfg"])
+@pytest.mark.parametrize(
+    "name, value",
+    [
+        ("n_workers", "auto"),
+        ("n_workers", 1),
+        ("n_workers", 2),
+        ("delay", 0.1),
+        ("delay", 1),
+        ("parallel_backend", "threads"),
+        ("parallel_backend", "processes"),
+        ("parallel_backend", "unknown_backend"),
+    ],
+)
+def test_reading_values_from_config_file(tmp_path, config_file, name, value):
+    config = f"""
+    [pytask]
+    {name} = {value}
+    """
+    tmp_path.joinpath(config_file).write_text(textwrap.dedent(config))
+
+    try:
+        session = main({"paths": tmp_path})
+    except Exception as e:
+        assert "Error while configuring pytask" in str(e)
+    else:
+        assert session.exit_code == 0
+        if value == "auto":
+            value = os.cpu_count() - 1
+        assert session.config[name] == value

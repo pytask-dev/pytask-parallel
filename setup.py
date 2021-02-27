b061dae620e50dc5a977679b2f83207472029212
Tobias Raabe <raabe@posteo.de>
2021-02-27 09:55:41 +0100

    last adjustments.

--- setup.py
+++ setup.py
@@ -1,16 +1,33 @@
+from pathlib import Path
+
 from setuptools import find_packages
 from setuptools import setup
 
 import versioneer
+
+
+README = Path("README.rst").read_text()
+
+PROJECT_URLS = {
+    "Documentation": "https://github.com/pytask-dev/pytask-latex",
+    "Github": "https://github.com/pytask-dev/pytask-latex",
+    "Tracker": "https://github.com/pytask-dev/pytask-latex/issues",
+    "Changelog": "https://github.com/pytask-dev/pytask-latex/blob/main/CHANGES.rst",
+}
+
 
 setup(
     name="pytask-parallel",
     version=versioneer.get_version(),
     cmd_class=versioneer.get_cmdclass(),
     description="Parallelize the execution of pytask.",
+    long_description=README,
+    long_description_content_type="text/x-rst",
     author="Tobias Raabe",
     author_email="raabe@posteo.de",
     python_requires=">=3.6",
+    url=PROJECT_URLS["Github"],
+    project_urls=PROJECT_URLS,
     license="MIT",
     classifiers=[
         "Development Status :: 3 - Alpha",

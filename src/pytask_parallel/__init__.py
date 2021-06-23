44db1f0b4c87d36ca87817495608a448d92529b1
Tobias Raabe <raabe@posteo.de>
2021-06-23 08:31:31 +0200

    Replace versioneer with setuptools-scm. (#24)

--- src/pytask_parallel/__init__.py
+++ src/pytask_parallel/__init__.py
@@ -1,5 +1,9 @@
-"""The entry-point for pytask-parallel."""
-from ._version import get_versions
+try:
+    from ._version import version as __version__
+except ImportError:
+    # broken installation, we don't even try unknown only works because we do poor mans
+    # version compare
+    __version__ = "unknown"
 
-__version__ = get_versions()["version"]
-del get_versions
+
+__all__ = ["__version__"]

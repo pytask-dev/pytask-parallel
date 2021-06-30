054b0ffe0852b6eb40d0b027484af72e417dfdd3
Tobias Raabe <raabe@posteo.de>
2021-06-30 22:43:40 +0200

    add.

--- src/pytask_parallel/logging.py
+++ src/pytask_parallel/logging.py
@@ -1,6 +1,6 @@
 """Contains code relevant to logging."""
-import click
 from _pytask.config import hookimpl
+from _pytask.console import console
 
 
 @hookimpl(trylast=True)
@@ -8,4 +8,4 @@ def pytask_log_session_header(session):
     """Add a note for how many workers are spawned."""
     n_workers = session.config["n_workers"]
     if n_workers > 1:
-        click.echo(f"Started {n_workers} workers.")
+        console.print(f"Started {n_workers} workers.")

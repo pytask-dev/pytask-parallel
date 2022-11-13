70384ff6689fc911b08764f77108d2fa4b6533ce
Tobias Raabe <raabe@posteo.de>
2022-11-13 10:26:16 +0100

    Remove code and tests for ini configs.

--- tests/test_config.py
+++ tests/test_config.py
@@ -27,9 +27,6 @@ def test_interplay_between_debugging_and_parallel(tmp_path, pdb, n_workers, expe
 
 @pytest.mark.end_to_end
 @pytest.mark.parametrize(
-    "config_file", ["pytask.ini", "tox.ini", "setup.cfg", "pyproject.toml"]
-)
-@pytest.mark.parametrize(
     "configuration_option, value, exit_code",
     [
         ("n_workers", "auto", ExitCode.OK),
@@ -43,26 +40,17 @@ def test_interplay_between_debugging_and_parallel(tmp_path, pdb, n_workers, expe
     ],
 )
 def test_reading_values_from_config_file(
-    tmp_path, capsys, config_file, configuration_option, value, exit_code
+    tmp_path, capsys, configuration_option, value, exit_code
 ):
-    if config_file == "pyproject.toml":
-        config = f"""
-        [tool.pytask.ini_options]
-        {configuration_option} = {value!r}
-        """
-    else:
-        config = f"""
-        [pytask]
-        {configuration_option} = {value}
-        """
-    tmp_path.joinpath(config_file).write_text(textwrap.dedent(config))
+    config = f"""
+    [tool.pytask.ini_options]
+    {configuration_option} = {value!r}
+    """
+    tmp_path.joinpath("pyproject.toml").write_text(textwrap.dedent(config))
 
     session = main({"paths": tmp_path})
     captured = capsys.readouterr()
-    if config_file == "pyproject.toml":
-        assert "WARNING" not in captured.out
-    else:
-        assert "WARNING" in captured.out
+    assert "WARNING" not in captured.out
 
     assert session.exit_code == exit_code
     if value == "auto":

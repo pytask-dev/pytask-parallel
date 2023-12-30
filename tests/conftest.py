37ed8ff163a1412a05323a8d265c4daa3ec0d2ae
Tobias Raabe <raabe@posteo.de>
2023-12-30 15:41:48 +0100

    Disable errors with nbmake on macos.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,13 @@
 from __future__ import annotations
 
+import os
 import sys
 from contextlib import contextmanager
 from typing import Callable
 
 import pytest
 from click.testing import CliRunner
+from nbmake.pytest_items import NotebookItem
 from pytask import storage
 
 
@@ -71,3 +73,11 @@ class CustomCliRunner(CliRunner):
 @pytest.fixture()
 def runner():
     return CustomCliRunner()
+
+
+def pytest_collection_modifyitems(session, config, items) -> None:  # noqa: ARG001
+    """Add markers to Jupyter notebook tests."""
+    if sys.platform == "darwin" and "CI" in os.environ:  # pragma: no cover
+        for item in items:
+            if isinstance(item, NotebookItem):
+                item.add_marker(pytest.mark.xfail(reason="Fails regularly on MacOS"))
